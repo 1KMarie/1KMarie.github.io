4dc5f19927592e9252420a524220b92bb6f99d37
1KMarie <115493624+1KMarie@users.noreply.github.com>
2022-10-12 14:48:38 -0400

    UD

---
+++ circle2.js
@@ -0,0 +1,35 @@
+function setup() {
+    createCanvas(600,400);
+    background(0)
+
+}
+
+px=0
+py=0
+
+vx = 1
+vy = 1
+
+radius=30
+
+function draw(){
+    background(0)
+    
+    px = px + vx
+    py = py + vy
+
+    if(px > 600|| px<0){
+        vx=-vx
+    }
+
+    if(py > 400 || py< 0){
+        vy=-vy
+    }
+
+    ellipse(px, py, radius, radius)
+
+
+
+    // ellipse(px+vx,py+vy,radius, radius);
+
+}
